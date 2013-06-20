2a13daf8fbdbfbfb010ac443ab99487fa5e16af3
ta9o <im.ta9o@gmail.com>
2013-06-21 01:54:07 +0900

    Initial Commit

---
+++ LoveDoorTests/LoveDoorTests.h
@@ -0,0 +1,13 @@
+//
+//  LoveDoorTests.h
+//  LoveDoorTests
+//
+//  Created by Takuo IMBE on 6/21/13.
+//  Copyright (c) 2013 Takuo IMBE. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LoveDoorTests : SenTestCase
+
+@end
---
+++ LoveDoorTests/LoveDoorTests.m
@@ -0,0 +1,32 @@
+//
+//  LoveDoorTests.m
+//  LoveDoorTests
+//
+//  Created by Takuo IMBE on 6/21/13.
+//  Copyright (c) 2013 Takuo IMBE. All rights reserved.
+//
+
+#import "LoveDoorTests.h"
+
+@implementation LoveDoorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LoveDoorTests");
+}
+
+@end
